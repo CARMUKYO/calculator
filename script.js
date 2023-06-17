c1e6f1fa271ffcf0ecb6e54f7937caf6c6205815
carmukyo <carlmike.aguila@gmail.com>
2023-06-17 15:43:36 +0800

    Adds keyboard support, decimal operator, and backspace. Fix Bug where if equal is pressed before a operator it returns a result

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const numbers = document.querySelectorAll(".number");
+const dot = document.getElementById(".");
 const operators = document.querySelectorAll(".operators");
 const equal = document.getElementById("equal");
 const clear = document.getElementById("clear");
+const backSpace = document.getElementById("back");
 const solution = document.getElementById("solution");
 const result = document.getElementById("result");
-
+const buttons = document.querySelectorAll(".button");
 
 let firstNumber = 0;
 let operator ="";
@@ -16,12 +18,15 @@ result.textContent = "";
 numbers.forEach((num) =>{
     num.addEventListener("click", addNumber);
 });
+
 operators.forEach((operator) => {
     operator.addEventListener("click", setOperator);
 });
 
 equal.addEventListener("click", evaluateEquation);
 clear.addEventListener("click", clearCal);
+dot.addEventListener("click", addNumber);
+backSpace.addEventListener("click", backFunction);
 
 function addNumber(n){
     displayArray.push(n.target.id);
@@ -36,8 +41,11 @@ function setOperator(n){
 }
 
 function evaluateEquation(){
+    if(solution.textContent === ""){
+        return;
+    }
     secondNumber = result.textContent;
-    solution.textContent = solution.textContent + operator + secondNumber;
+    solution.textContent = firstNumber + operator + secondNumber;
     result.textContent = operate(firstNumber,secondNumber,operator);
 }
 
@@ -56,7 +64,7 @@ function mutiply(firstNumber,secondNumber){
 
 function divide(firstNumber,secondNumber){
     if (Number(secondNumber) === 0) {
-        return "Infinity";
+        return "ERRORRRRRRRRRRRR";
     }
     return Number(firstNumber) / Number(secondNumber);
 }
@@ -73,7 +81,6 @@ function operate(firstNumber,secondNumber,operator){
     }   
 }
 
-
 function clearCal(){
     firstNumber = 0;
     secondNumber = 0;
@@ -81,4 +88,30 @@ function clearCal(){
     displayArray.length = 0;
     solution.textContent = "";
     result.textContent = "";
+}
+
+document.addEventListener('keydown', takeKeyboardInput);
+function takeKeyboardInput(n){
+    buttons.forEach((button) => {
+        if (button.id == n.key) {
+            document.getElementById(button.id).click();
+        }
+    });
+    if(n.key == 'Enter') {
+        evaluateEquation();
+    }else if(n.key == "c"){
+        clearCal();
+    }else if(n.key == "Backspace"){
+        backFunction();
+    }
+}
+
+function backFunction(){
+    if (displayArray.length > 1) {
+        displayArray.pop();
+        result.textContent = displayArray.join('');
+        } else {
+            displayArray.length = 0;
+            result.textContent = "0";
+        }
 }
